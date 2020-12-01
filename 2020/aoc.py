4a3b142a54bbd225cb80c5df9f20ba5769f949fd
Luisa Lourenço <luisa.lourenco@outsystems.com>
2020-12-01 07:50:27 +0000

    First day solution

--- 2020/aoc.py
+++ 2020/aoc.py
@@ -15,10 +15,29 @@ sys.path.insert(0, FILE_DIR + "/../../")
 from common.utils import read_input, main, clear  # NOQA: E402
 
 def day1_1(data):
-    return data
+    sum = 2020
+    data = sorted(data, key=int)
+
+    for i in range(0, len(data)):
+        elem1 = int(data[i])
+        for j in range(i, len(data)):
+            elem2 = int(data[j])
+            if (elem1 + elem2 == sum):
+                 return elem1 * elem2
 
 def day1_2(data):
-    return data
+    sum = 2020
+    data = sorted(data, key=int)
+
+    for i in range(0, len(data)):
+        elem1 = int(data[i])
+        for j in range(i, len(data)):
+            elem2 = int(data[j])
+            for k in range(j, len(data)):
+                elem3 = int(data[k])
+                if (elem1 + elem2 + elem3 == sum):
+                    result = elem1 * elem2 * elem3
+    return result
 
 if __name__ == "__main__":
     main(sys.argv, globals(), 2020)
