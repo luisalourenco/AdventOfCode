f942d8fa1eb2e2a3e0ccbae7ba10f0c98d1dcdc5
Luisa Lourenço <luisa.lourenco@outsystems.com>
2020-12-02 08:08:07 +0000

    Day 2 solution

--- 2020/aoc.py
+++ 2020/aoc.py
@@ -14,6 +14,21 @@ sys.path.insert(0, FILE_DIR + "/../")
 sys.path.insert(0, FILE_DIR + "/../../")
 from common.utils import read_input, main, clear  # NOQA: E402
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+WHITE_SQUARE = "█"
+WHITE_CIRCLE = "•"
+BLUE_CIRCLE = f"{bcolors.OKBLUE}{bcolors.BOLD}•{bcolors.ENDC}"
+RED_SMALL_SQUARE = f"{bcolors.FAIL}{bcolors.BOLD}■{bcolors.ENDC}"
+
 def day1_1(data):
     sum = 2020
     data = sorted(data, key=int)
@@ -38,6 +53,34 @@ def day1_2(data):
                 if (elem1 + elem2 + elem3 == sum):
                     result = elem1 * elem2 * elem3
     return result
+
+def day2_1(data):
+    validPasswords = 0
+    for inputLine in data:
+        input = inputLine.split(" ")
+        letter = input[1][0]
+        min = int(input[0].split("-")[0])
+        max = int(input[0].split("-")[1])
+        occurrences = input[2].count(letter)
+
+        if occurrences <= max and occurrences >= min:
+            validPasswords += 1
+
+    return validPasswords        
+
+def day2_2(data):
+    validPasswords = 0
+    for inputLine in data:
+        input = inputLine.split(" ")
+        letter = input[1][0]
+        pos1 = int(input[0].split("-")[0])-1
+        pos2 = int(input[0].split("-")[1])-1
+               
+        if bool(input[2][pos1] == letter) ^ bool(input[2][pos2] == letter):
+            validPasswords += 1
+
+    return validPasswords
+
 
 if __name__ == "__main__":
     main(sys.argv, globals(), 2020)
