5ae56e452a2989767a6eafb2e228b112f9435fac
Luísa Lourenço <luisa.lourenco@outsystems.com>
2022-11-28 13:40:55 +0000

    Prepare Day 1 (2022)

---
+++ 2022/aoc.py
@@ -0,0 +1,86 @@
+# Based on template from https://github.com/scout719/adventOfCode/
+# -*- coding: utf-8 -*
+# pylint: disable=import-error
+# pylint: disable=unused-import
+# pylint: disable=wildcard-import
+# pylint: disable=wrong-import-position
+# pylint: disable=consider-using-enumerate-
+
+from io import DEFAULT_BUFFER_SIZE
+from threading import current_thread
+from timeit import default_timer as timer
+from collections import deque
+import functools
+import math
+import os
+import sys
+import time
+import copy
+import re
+import itertools
+from typing import ChainMap, DefaultDict
+import numpy as np
+from functools import lru_cache
+import operator
+from itertools import takewhile
+import itertools, collections
+from turtle import Turtle, Screen, heading, left
+from math import exp, pi, remainder, sqrt
+from collections import namedtuple
+from collections import Counter
+from collections import defaultdict
+from numpy.lib.arraypad import pad
+from termcolor import colored
+import termcolor
+import random
+
+# UPDATE THIS VARIABLE
+AOC_EDITION_YEAR = 2022
+
+FILE_DIR = os.path.dirname(os.path.realpath(__file__))
+print(FILE_DIR)
+sys.path.insert(0, FILE_DIR + "/")
+sys.path.insert(0, FILE_DIR + "/../")
+sys.path.insert(0, FILE_DIR + "/../../")
+
+DEBUG_MODE = False
+
+from common.mathUtils import *
+from common.utils import *# read_input, main, clear, AssertExpectedResult, ints, printGridsASCII  # NOQA: E402
+from common.mapUtils import printMap, buildMapGrid, buildGraphFromMap
+from common.graphUtils import dijsktra, printGraph, find_all_paths, find_path, find_shortest_path, find_shortest_pathOptimal, bfs, dfs, Graph, hashable_lru
+from common.aocVM import *
+from lark import Lark, Transformer, v_args, UnexpectedCharacters, UnexpectedToken
+from pyformlang.cfg import Production, Variable, Terminal, CFG, Epsilon
+from itertools import islice
+
+# pylint: enable=import-error
+# pylint: enable=wrong-import-position
+
+
+
+################# Advent of Code - 2022 Edition #################
+
+#region
+##### Day 1 #####
+
+#Day 1, part 1: 
+#Day 1, part 2: 
+def day1_1(data):
+    #data = read_input(2022, "1")    
+    result = 0
+    
+    for line in data:
+        n = int(line)
+        result += n      
+    
+    AssertExpectedResult(0, result)
+    return result
+
+#endregion
+
+
+if __name__ == "__main__":
+    # override timeout
+    main(sys.argv, globals(), AOC_EDITION_YEAR, 900)
+
